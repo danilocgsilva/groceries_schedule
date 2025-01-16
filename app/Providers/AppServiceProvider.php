178ed69a893e11c0fb90f2233cd496f1f2bcc61a
Danilo Silva <contact@danilocgsilva.me>
2025-01-16 12:26:42 -0300

    Added blade directives
    Longs classes strings from tailwind for form elements has been replaced
    by custom blade directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,43 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive(
+            'form_input_classes', 
+            fn () => $this->getFormInputClasses()
+        );
+
+        Blade::directive(
+            'form_submit_classes', 
+            fn () => $this->getFormSubmitClasses()
+        );
+
+        Blade::directive(
+            'form_action_classes', 
+            fn () => $this->getFormActionClasses()
+        );
+    }
+
+    private function getFormInputClasses(): string
+    {
+        $basicClasses = "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5";
+        $darkClasses = "dark:bg-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+
+        return $basicClasses . " " .  $darkClasses;
+    }
+
+    public function getFormSubmitClasses(): string
+    {
+        $basicClasses = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg sm:w-auto px-5 py-2.5 text-center cursor-pointer";
+        $darkThemeClasses = "dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800";
+        
+        return $basicClasses . " " . $darkThemeClasses;
+    }
+
+    public function getFormActionClasses(): string
+    {
+        $basicClasses = "bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg sm:w-auto px-5 py-2.5 text-center cursor-pointer";
+        $darkThemeClasses = "dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800";
+        
+        return $basicClasses . " " . $darkThemeClasses;
     }
 }
--- resources/views/GroceryItem/edit.blade.php
+++ resources/views/GroceryItem/edit.blade.php
@@ -8,66 +8,24 @@
     "textContent" => "Change some data from the grocery {$groceryItem->name}."
 ])
 
-<form action="{{ route('grocery_items.update', ['grocery_item' => $groceryItem->id ]) }}" method="POST">
-    <label for="name" class="
-        block
-        font-medium
-        px-2
-        ">
-        Name
-    </label>
-    <input type="text" name="name" id="name" class="
-        border 
-        rounded-lg
-        py-2 
-        px-3 
-        text-gray-700 
-        focus:outline-none
-        block
-    " value="{{ $groceryItem->name }}" />
+<form class="max-w-2xl mx-auto" action="{{ route('grocery_items.update', ['grocery_item' => $groceryItem->id ]) }}" method="POST">
+    <div class="mb-5">
+        <label for="name" class="block font-medium px-2">Name</label>
+        <input type="text" name="name" id="name" class="@form_input_classes" value="{{ $groceryItem->name }}" />
+    </div>
 
-    <label for="lasting_estimate" class="
-        block
-        font-medium
-        px-2
-        ">
-        Estimate
-    </label>
-    <input type="text" name="lasting_estimate" id="lasting_estimate" class="
-        border 
-        rounded-lg
-        py-2 
-        px-3 
-        text-gray-700 
-        focus:outline-none
-        block
-    " placeholder="Estimate lasting" />
+    <div class="mb-5">
+        <label for="lasting_estimate" class="block font-medium px-2">Estimate</label>
+        <input type="text" name="lasting_estimate" id="lasting_estimate" class="@form_input_classes" placeholder="Estimate lasting" />
+    </div>
 
-    <a href="#" class="
-        border 
-        rounded-lg
-        py-2 
-        px-3 
-        text-gray-700 
-        focus:outline-none
-    ">Set first date now!</a>
-    <br />
+    <div class="mb-5">
+        <a href="#" class="@form_action_classes">Set first date now!</a>
+    </div>
 
     @method("PUT")
     @csrf
-    <input type="submit" value="update" class="
-        border 
-        hover:bg-blue-800 
-        focus:ring-blue-300 
-        font-medium 
-        rounded-lg 
-        sm:w-auto 
-        py-2 
-        px-6 
-        text-center 
-        dark:hover:bg-blue-700 
-        dark:focus:ring-blue-800
-    " />
+    <input type="submit" value="Update" class="@form_submit_classes" />
 </form>
 
 @endsection
