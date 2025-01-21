0f9a476ffe56ef716265f13deedec9c1fded020d
Danilo Silva <contact@danilocgsilva.me>
2025-01-21 20:54:41 -0300

    wip: adding purchase

---
+++ app/Models/Purchase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    protected $table = "purchases";
+
+    protected $fillable = [
+        "grocery_item_id",
+        "amount"
+    ];
+}
