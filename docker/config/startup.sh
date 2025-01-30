580a1261de96e99a6ee391afbff018b213f337bb
Danilo Silva <contact@danilocgsilva.me>
2025-01-30 14:25:25 -0300

    Fine tune to the starting script in shell

--- docker/config/startup.sh
+++ docker/config/startup.sh
@@ -2,6 +2,4 @@
 
 a2enmod rewrite
 service apache2 start
-cd /var/www
-chown -Rv www-data:www-data storage
-npm run dev
+npm composer dev
---
+++ startup_first_time.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chown -Rv www-data:www-data storage
+mysql -uroot -pgroceries_schedule_db -h
+mysql -uroot -pgroceries_schedule_db -h
